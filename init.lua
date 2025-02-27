01551ba64c2ee88807e48644329dc91fa354e035
DFTHLM <mstej.juhasik@gmail.com>
2025-02-27 23:27:02 +0100

    added lsp stuff

--- init.lua
+++ init.lua
@@ -26,3 +26,18 @@ vim.keymap.set('n', '<C-t>', builtin.treesitter, { desc = 'Telescope treesitter'
 
 vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
 
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+require("mason-lspconfig").setup_handlers {
+    function (server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {}
+    end,
+}
+
+
+vim.keymap.set('v', '<C-f>', function()
+  local selected_word = vim.fn.expand("<cword>")
+  builtin.lsp_references({ query = selected_word })
+end, { desc = "Search references to the selected word" })
+
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,10 +5,9 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          ensure_installed = "all",
           sync_install = false,
           highlight = { enable = true },
-          indent = { enable = true },  
         })
     end
  }
