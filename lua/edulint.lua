96d752b7ac1a44da64822d3b39f8546cb0ac5c89
DFTHLM <matej.juhasik@gmail.com>
2025-10-21 12:04:32 +0200

    added mypy and changed :Edulint to :Check

--- lua/edulint.lua
+++ lua/edulint.lua
@@ -5,12 +5,12 @@ local ns = vim.api.nvim_create_namespace("edulint")
 function M.run(bufnr)
     bufnr = bufnr or vim.api.nvim_get_current_buf()
     local filename = vim.api.nvim_buf_get_name(bufnr)
-    local output = vim.fn.systemlist("edulint check " .. filename)
+    local edulint = vim.fn.systemlist("edulint check " .. filename)
+    local mypy = vim.fn.systemlist("mypy --strict " .. filename)
 
     local diagnostics = {}
 
-    for _, line in ipairs(output) do
-        -- Adjust this to match edulint's output format
+    for _, line in ipairs(edulint) do
         local l, c, msg = line:match(":(%d+):(%d+): (.+)")
         if l and c and msg then
             table.insert(diagnostics, {
@@ -22,11 +22,27 @@ function M.run(bufnr)
         end
     end
 
+    --p1_database.py:15: error: Incompatible types in assignment (expression has type "tuple[list[str], list[list[Any]]]", variable has type "list[int]")  [assignment]
+    --p1_database.py:16: error: Incompatible return value type (got "list[int]", expected "list[str]")  [return-value]
+    --Found 2 errors in 1 file (checked 1 source file)
+
+    for _, line in ipairs(mypy) do
+        local l, msg = line:match(":(%d+): (.+)")
+        if l and  msg then
+            table.insert(diagnostics, {
+                lnum = tonumber(l) - 1,
+                col = 0,
+                message = msg,
+                severity = vim.diagnostic.severity.WARN,
+            })
+        end
+    end
+
     vim.diagnostic.set(ns, bufnr, diagnostics, {})
 end
 
 function M.setup()
-    vim.api.nvim_create_user_command("EduLint", function()
+    vim.api.nvim_create_user_command("Check", function()
         M.run(0)
     end, {})
 end
